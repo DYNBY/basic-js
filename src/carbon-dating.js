bd4afc5260e43c7c4286500a20a9554f82394527
dynby <dyominyuri@gmail.com>
2023-04-15 22:09:47 +0300

    carbon dating complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  let n = Number(sampleActivity);
+  if ((n <= 0)||(n >= 15)) return false;
+  if (Number.isNaN(n)) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / n) / (0.693 / HALF_LIFE_PERIOD));
+};
 
 module.exports = {
   dateSample
